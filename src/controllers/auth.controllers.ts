93ef1e81fc2ab9b04e3c57128510a5af2ea013a8
Liam <phuc01112002@gmail.com>
2024-04-30 16:44:21 +0700

    #7 - Liam code feature login

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -2,15 +2,35 @@ import { CLIENT_MESSAGE } from '@/constants/clientMessages'
 import { ParamsDictionary } from 'express-serve-static-core'
 import { Response, Request, NextFunction } from 'express'
 import asyncHandler from 'express-async-handler'
-import { RegisterBodyType, RegisterResType } from '@/schemaValidations/auth.schema'
+import { LoginBodyType, RegisterBodyType, RegisterResType } from '@/schemaValidations/auth.schema'
 import authService from '@/services/auth.services'
+import { User } from '@prisma/client'
+import { PHONE_VERIFY } from '@/enums/userStatus'
 
 export const registerController = asyncHandler(
-  async (req: Request<ParamsDictionary, any, RegisterBodyType>, res: Response<any>, next: NextFunction) => {
+  async (req: Request<ParamsDictionary, any, RegisterBodyType>, res: Response<RegisterResType>, next: NextFunction) => {
     const result = await authService.register(req.body)
     res.json({
       message: CLIENT_MESSAGE.REGISTER_SUCCESS,
-      result
+      data: {
+        ...result
+      }
+    })
+  }
+)
+
+export const loginController = asyncHandler(
+  async (req: Request<ParamsDictionary, any, LoginBodyType>, res: Response<any>, next: NextFunction) => {
+    const user = req.user as User
+    const { id } = user
+    const phone_verify = user.phone_verify as PHONE_VERIFY
+    const result = await authService.login({ user_id: id, phone_verify })
+
+    res.json({
+      message: CLIENT_MESSAGE.LOGIN_SUCCESS,
+      data: {
+        result
+      }
     })
   }
 )
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
-import { registerController } from '@/controllers/auth.controllers'
-import { registerMiddleware } from '@/middleware/auth.middleware'
+import { loginController, registerController } from '@/controllers/auth.controllers'
+import { loginMiddleware, registerMiddleware } from '@/middleware/auth.middleware'
 import { Router } from 'express'
 
 const authRouter = Router()
@@ -8,8 +8,16 @@ const authRouter = Router()
  * Description: Register an account
  * Path: /auth/register
  * Method: POST
- * Body: {name: string; password: string; phone: string; role: "USER" | "AGENT"}
+ * Body: {name: string; email: string; password: string; phone: string; role: "USER" | "AGENT"}
  */
 authRouter.post('/register', registerMiddleware, registerController)
+
+/**
+ * Description: Login a user
+ * Path: /auth/login
+ * Method: POST
+ * Body: {phone:string'; password: string}
+ */
+authRouter.post('/login', loginMiddleware, loginController)
 
 export default authRouter
