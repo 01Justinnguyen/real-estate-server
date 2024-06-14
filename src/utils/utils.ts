7b2eabd77c7e38e7dac0304f495432e167f90bb6
Liam <phuc01112002@gmail.com>
2024-06-15 00:08:03 +0700

    #42 - Code filter middleware to resolve body request

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,5 @@
+import { omit } from 'lodash'
+
 export type SelectObject = {
   [key: string]: boolean
 }
@@ -18,3 +20,17 @@ export function exclude<ObjData extends object, K extends keyof ObjData>(
   const entries = Object.entries(objData).filter(([key]) => !keys.includes(key as K))
   return Object.fromEntries(entries) as Omit<ObjData, K>
 }
+
+// export function excludeArrayData<ObjData extends object, Key extends keyof ObjData>(
+//   data: ObjData[],
+//   keys: Key[]
+// ): Omit<ObjData, Key>[] {
+//   return data.map((item) => omit(item, keys) as Omit<ObjData, Key>)
+// }
+
+// export function excludeArrayData<ObjData extends object, Key extends keyof ObjData>(
+//   data: ObjData[],
+//   keys: Key[]
+// ): Omit<ObjData, Key>[] {
+//   return data.map((item) => omit(item, keys) as Omit<ObjData, Key>)
+// }
